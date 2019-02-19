a217c84ba48e549708462c0c8c512b1a9570d080
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-19 11:56:24 +0000

    Refactored sub modules

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,9 +4,23 @@ import { LoginComponent } from './login/login.component';
 import { ForgetpassComponent } from './forgetpass/forgetpass.component';
 import { AuthComponent } from './auth.component';
 import { IonicModule } from '@ionic/angular';
+import { Routes, RouterModule } from '@angular/router';
+
+const childRoutes: Routes = [
+  {
+    path: '',
+    component: AuthComponent,
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'prefix' },
+      { path: 'login', component: LoginComponent },
+      { path: 'forgetPass', component: ForgetpassComponent }
+    ]
+  }
+];
 
 @NgModule({
   declarations: [AuthComponent, LoginComponent, ForgetpassComponent],
-  imports: [CommonModule, IonicModule]
+  imports: [CommonModule, IonicModule, RouterModule.forChild(childRoutes)],
+  exports: [RouterModule]
 })
 export class AuthModule {}
--- src/app/mainapp/mainapp.module.ts
+++ src/app/mainapp/mainapp.module.ts
@@ -3,10 +3,22 @@ import { CommonModule } from '@angular/common';
 import { AtmsComponent } from './atms/atms.component';
 import { MainappComponent } from './mainapp.component';
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
+const childRoutes: Routes = [
+  {
+    path: '',
+    component: MainappComponent,
+    children: [
+      { path: '', redirectTo: 'atms', pathMatch: 'prefix' },
+      { path: 'atms', component: AtmsComponent }
+    ]
+  }
+];
 
 @NgModule({
   declarations: [AtmsComponent, MainappComponent],
-  imports: [RouterModule, CommonModule, IonicModule]
+  imports: [CommonModule, IonicModule, RouterModule.forChild(childRoutes)],
+  exports: [RouterModule]
 })
 export class MainappModule {}
