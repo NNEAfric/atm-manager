2603af6ac83c366bc1e488fbd0fdeb05201805b1
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-19 12:23:01 +0000

    Refactored MainRouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,11 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'mainapp',
     pathMatch: 'full'
   },
   {
-    path: 'home',
+    path: 'mainapp',
     loadChildren: './mainapp/mainapp.module#MainappModule'
   },
   {
