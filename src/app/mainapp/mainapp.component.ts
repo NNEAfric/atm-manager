acefaaebf25496b5ec9b0a1a969c4d2dc8df04cc
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-19 11:46:12 +0000

    Removed boilerplate code

--- src/app/mainapp/mainapp.component.ts
+++ src/app/mainapp/mainapp.component.ts
@@ -3,12 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-mainapp',
   templateUrl: './mainapp.component.html',
-  styleUrls: ['./mainapp.component.scss'],
+  styleUrls: ['./mainapp.component.scss']
 })
 export class MainappComponent implements OnInit {
-
-  constructor() { }
+  public appPages = [
+    {
+      title: 'Home',
+      url: '/home',
+      icon: 'home'
+    },
+    {
+      title: 'List',
+      url: '/list',
+      icon: 'list'
+    }
+  ];
+  constructor() {}
 
   ngOnInit() {}
-
 }
--- src/app/mainapp/mainapp.module.ts
+++ src/app/mainapp/mainapp.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { AtmsComponent } from './atms/atms.component';
 import { MainappComponent } from './mainapp.component';
 import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AtmsComponent, MainappComponent],
-  imports: [CommonModule, IonicModule]
+  imports: [RouterModule, CommonModule, IonicModule]
 })
 export class MainappModule {}
