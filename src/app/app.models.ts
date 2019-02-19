fc65cd57356215dc7272cee6c9f584ae15910d80
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-19 16:32:56 +0000

    Added Pagination Model

--- src/app/app.models.ts
+++ src/app/app.models.ts
@@ -5,8 +5,45 @@ export interface AuthResponse {
   expires_in: number;
 }
 
+export interface PaginatedData<T> extends Data<T[]> {
+  links: Links;
+  meta: Meta;
+}
+
 export interface Data<T> {
   data: T;
+}
+
+export interface ATMData {
+  id: number;
+  name: string;
+  status: number;
+  city: string;
+  branch: string;
+  coordinate: Coordinate;
+  updated_at: Date;
+}
+
+export interface Coordinate {
+  lat: number;
+  lng: number;
+}
+
+interface Meta {
+  current_page: number;
+  from: number;
+  last_page: number;
+  path: string;
+  per_page: number;
+  to: number;
+  total: number;
+}
+
+interface Links {
+  first: string;
+  last: string;
+  prev: string;
+  next: string;
 }
 
 export interface UserData {
