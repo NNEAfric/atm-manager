e79d548f52a965b179b7cdd1bfd35e74d7766643
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-19 15:42:43 +0000

    Configured Guards

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,14 +1,38 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+  CanActivateChild
+} from '@angular/router';
+import { AuthService } from '../providers/auth.service';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthGuard implements CanActivate {
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+@Injectable()
+export class AuthGuard implements CanActivate, CanActivateChild {
+  constructor(private router: Router, public authSvc: AuthService) {}
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (!this.authSvc.isTokenExpired()) {
+      return true;
+    }
+
+    // not logged in so redirect to login page
+    this.router.navigate(['/auth/login'], {
+      queryParams: { returnUrl: state.url }
+    });
+    return false;
+  }
+
+  canActivateChild(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (!this.authSvc.isTokenExpired()) {
+      return true;
+    }
+
+    // not logged in so redirect to login page
+    this.router.navigate(['/auth/login'], {
+      queryParams: { returnUrl: state.url }
+    });
+    return false;
   }
 }
--- src/app/guards/non-auth.guard.ts
+++ src/app/guards/non-auth.guard.ts
@@ -1,14 +1,28 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, Router, CanActivateChild } from '@angular/router';
+import { AuthService } from '../providers/auth.service';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class NonAuthGuard implements CanActivate {
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+@Injectable()
+export class NonAuthGuard implements CanActivate, CanActivateChild {
+  constructor(private router: Router, private authSvc: AuthService) {}
+
+  canActivate() {
+    if (this.authSvc.isTokenExpired()) {
+      return true;
+    }
+
+    // logged in so redirect to home page
+    this.router.navigate(['/mainapp/atms']);
+    return false;
+  }
+
+  canActivateChild() {
+    if (this.authSvc.isTokenExpired()) {
+      return true;
+    }
+
+    // logged in so redirect to home page
+    this.router.navigate(['/mainapp/atms']);
+    return false;
   }
 }
